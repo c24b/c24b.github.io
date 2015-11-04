0804abbfafe31af207dc0219959d0b425073c7e3
Constance de Quatrebarbes <4barbes@gmail.com>
2015-11-04 13:26:02 +0100

    COURS3

--- numi/rw_demo2.py
+++ numi/rw_demo2.py
@@ -1,14 +1,16 @@
-def read_csv_file(filename):
-    #lire une à une les lignes d'un fichier
-    with open(filename, 'w') as f:
-        for line in f:
-            f.readline(line)
+
+import csv
+    
+def read_csv(file='eggs.csv'):
+    with open(file, 'rb') as csvfile:
+    spamreader = csv.reader(csvfile, delimiter=';')
+    for row in spamreader:
+        print ', '.join(row)
     return
     
-def write_csv_file(filename, data):
+def write_csv(filename="eggs.csv, data= ['Spam', 'Lovely Spam', 'Wonderful Spam']):
     #ecrire une liste de données dans un fichier avec pour séparateur ";"
-    with open(filename, 'w') as f:
-        for line in data
-            line = line.join(";")
-            f.writeline(line)
+    with open('eggs.csv', 'wb') as csvfile:
+    spamwriter = csv.writer(csvfile, delimiter=';')
+    spamwriter.writerow(data)
     return
